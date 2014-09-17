2508dc6a3b2965cbf4051cce2561b64504e05ebb
Delu Anachebe <cjaf.javacakess@gmail.com>
2014-09-17 21:05:12 +0100

    i wish i could into #ifdef sad smiley

--- src/constants.h
+++ src/constants.h
@@ -1,7 +1,8 @@
 #include <curses.h>
 
-#ifdef _WIN32 || __linux__ || __FreeBSD__ /* Not sure about Windows and BSD...*/
+#if defined(_WIN32) || defined(__linux__) || defined(__FreeBSD__)
+    /* Not sure about Windows and BSD...*/
     #define CKEY_BACKSPACE KEY_BACKSPACE
-#elif __APPLE__ || __MACH__               /* Both are the same */
+#elif defined(__APPLE__) || defined(__MACH__) /* Both are the same */
     #define CKEY_BACKSPACE 127
 #endif
